f9832191700d24072a2adc2098803d6b773e1448
viviruffy0304 <59783074+viviruffy0304@users.noreply.github.com>
2020-02-18 15:36:00 +0900

    Update carrierwave.rb

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -14,7 +14,7 @@ CarrierWave.configure do |config|
       aws_secret_access_key: Rails.application.credentials.aws[:secret_access_key],  #credentails.ymlに鍵の本体があります
       region: 'ap-northeast-1'
     }
-    config.fog_directory  = #'furima68'
-    config.asset_host = 'https://s3-ap-northeast-1.amazonaws.com/tech54-mercari-a'
+    config.fog_directory  = 'furima68'
+    config.asset_host = 'https://s3-ap-northeast-1.amazonaws.com/furima68'
   end
 end
