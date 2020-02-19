94285390a66b863ccb0c1750b5399564e6fe6e65
viviruffy0304 <59783074+viviruffy0304@users.noreply.github.com>
2020-02-19 20:30:57 +0900

    controll

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,5 @@
 Rails.application.routes.draw do
 
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-
   root to: "users#howToPay"
   # get 'users/logout' to: 'user#logout'
   resources :informations, only: [:index]
