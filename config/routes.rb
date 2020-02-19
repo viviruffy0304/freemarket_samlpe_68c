4478fbd9cd416ccf91d373252de8f4d129362c36
viviruffy0304 <59783074+viviruffy0304@users.noreply.github.com>
2020-02-19 20:56:42 +0900

    Update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
 
-  root to: "users#index"
+  root to: "users#logout"
   resources :informations, only: :index
   resources :tests, only: :show
   resources :users, only: :index do
