eed26c6ca67a61f57130303daf1326258c40e09d
Shirane70 <shirane.naoki.tree@ezweb.ne.jp>
2020-02-19 19:18:56 +0900

    rooting-changed

--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,15 @@
 Rails.application.routes.draw do
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-
-  root to: "users#howToPay"
+  root to: "informations#index"
+  # root to: "users#howToPay"
   # get 'users/logout' to: 'user#logout'
   resources :informations, only: [:index , :show]
   # resources :users, only: [:index]
-  resources :users do
-    collection do
-      get :logout
-      get :howToPay
-    end
-  end 
+  #resources :users do
+  #  collection do
+  #    get :logout
+  #    get :howToPay
+  #  end
+  #end 
 end
