2f9fda1ad84851f371b8a1a868ed6879080d2db7
Shirane70 <shirane.naoki.tree@ezweb.ne.jp>
2020-02-20 10:34:48 +0900

    ^haml

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
 
   root to: "users#index"
-  resources :informations, only: :index
+  resources :informations, only: :index , :show
   resources :tests, only: :show
   resources :users, only: :index do
     collection do
