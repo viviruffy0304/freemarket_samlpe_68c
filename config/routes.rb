51886ae781aa0eba971556e6fa523ffd9ee4db75
タクミ <toldend@icloud.com>
2020-02-21 13:28:27 +0900

    fileExtension was fixed and sellPage_front was interrupted on the way

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,9 @@ Rails.application.routes.draw do
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-  root to: "users#howToPay"
+  root to: "products#index"
   # get 'users/logout' to: 'user#logout'
+  resources :products, only: [:index]
   resources :informations, only: [:index]
   # resources :users, only: [:index]
   resources :users do
