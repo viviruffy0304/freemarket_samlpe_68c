963b9cb92997c0365095683a1fec8e3ea3c32140
タクミ <toldend@icloud.com>
2020-02-21 13:32:58 +0900

    topPage_bug was fixed

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,9 @@ Rails.application.routes.draw do
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-  root to: "products#index"
+  # root to: "products#index"
   # get 'users/logout' to: 'user#logout'
+  root to: "informations#index"
   resources :products, only: [:index]
   resources :informations, only: [:index]
   # resources :users, only: [:index]
