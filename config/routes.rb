9ce7d19452ec53591503f9fd67f065b3c69b166d
viviruffy0304 <59783074+viviruffy0304@users.noreply.github.com>
2020-02-25 18:48:37 +0900

    card

--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,14 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   root to: "signup_users#index"
-  resources :products, only: [:index]
+  resources :products, only: [:index, :new]
   resources :informations, only: [:index , :show]
-  resources :tests, only: :show
+  resources :tests, only: :show do
+    collection do
+      get :pay
+      get :done
+    end
+  end 
   resources :signup_users, only: :new 
   resources :users, only: :index do
     collection do
