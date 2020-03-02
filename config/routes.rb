ef6fdc4f2f53e8b12b631070c15847e543c65d3b
Shirane70 <shirane.naoki.tree@ezweb.ne.jp>
2020-03-02 14:58:31 +0900

    Update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
 
-  root to: "signup_users#index"
+  root to: "informations#index"
   resources :products, only: [:index, :new]
   resources :informations, only: [:index , :show]
   resources :tests, only: :show do
