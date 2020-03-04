4c40b6fa9a684e3e22104feb5cef603b32391d32
Shirane70 <shirane.naoki.tree@ezweb.ne.jp>
2020-03-04 12:26:00 +0900

    edit-update-0304

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -3,11 +3,17 @@ class ProductsController < ApplicationController
   end
 
   def new
-    # @item = Item.new
+    #@item = Item.new
   end
 
   def edit
     #@item = Item.find(params[:id])
     
+  end
+
+  def update
+    #item = Item.find(params[:id])
+    #item.update(item_params)
+    #redirect_to item_path(item.id)
   end
 end
