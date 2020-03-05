cd6dc01a0f6cf99aa5c95f649ffd01f39f800714
Shirane70 <shirane.naoki.tree@ezweb.ne.jp>
2020-03-05 16:19:59 +0900

    redirect

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -14,6 +14,7 @@ class ItemsController < ApplicationController
   def update
     item = Item.find(params[:id])
     item.update(item_params)
+    redirect_to information_path(item.id)
   end
 
   private
