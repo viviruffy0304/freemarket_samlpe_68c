eed65429f4a7cc05f2cd1753eecf6044fb88ce38
Shirane70 <shirane.naoki.tree@ezweb.ne.jp>
2020-03-05 16:01:12 +0900

    edit-create

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -15,4 +15,9 @@ class ItemsController < ApplicationController
     item = Item.find(params[:id])
     item.update(item_params)
   end
+
+  private
+  def item_params
+    params.require(:item).permit(:brand, :category, :name, :description, :state, :postage, :region, :shipping_days, :price,)
+  end
 end
