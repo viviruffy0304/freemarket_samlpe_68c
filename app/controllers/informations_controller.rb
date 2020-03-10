1db6b109c4dc68da5f2f2b390a469c472a062458
Shirane70 <shirane.naoki.tree@ezweb.ne.jp>
2020-03-10 19:42:05 +0900

    restart

--- app/controllers/informations_controller.rb
+++ app/controllers/informations_controller.rb
@@ -8,7 +8,7 @@ class InformationsController < ApplicationController
 
   def show
     @user = User.find(@item.seller_id)
-    @category = Category.find(params[:id])
+    #@category = Category.find(params[:id])
   end
 
   def destroy
